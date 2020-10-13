3f2415d441ec9f6a0609ab5b66a07d05b620975e
lawrencejd1 <60728037+lawrencejd1@users.noreply.github.com>
2020-10-13 00:25:40 -0600

    Delete tweet.py

--- tweet.py
+++
@@ -1,9 +0,0 @@
-'''
-Turns tweet into a more simpler object to grab specific data from
-'''
-class Tweet:
-
-    def __init__(self, id, likes, text):
-        self.id = id
-        self.likes = likes
-        self.text = text
